2de223e159d1ce11a714ebcec74f2f394bacb276
BuildTools <unconfigured@null.spigotmc.org>
2017-08-17 18:33:42 -0500

    circle ci

--- example-app/main.py
+++ example-app/main.py
@@ -1,10 +1,4 @@
-from Tkinter import *           # Importing the Tkinter (tool box) library 
-root = Tk()                     # Create a background window
-                                # Create a list
-li = 'Carl Patrick Lindsay Helmut Chris Gwen'.split()
-listb = Listbox(root)           # Create a listbox widget
-for item in li:                 # Insert each item within li into the listbox
-    listb.insert(0,item)
+import time
 
-listb.pack()                    # Pack listbox widget
-root.mainloop()
+print('Hello, world!')
+time.sleep(10)
